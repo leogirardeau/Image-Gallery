90318a355b92a4ed2333c859c21213beda87ec9a
leogirardeau <leogir.pro@gmail.com>
2020-08-14 17:53:04 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function imageGallery() {
+    const highlight = document.querySelector('.gallery-highlight');
+    const previews = document.querySelectorAll('.room-preview img');
+
+    previews.forEach(preview => {
+        preview.addEventListener('click', function () {
+        });
+    });
+}
+
+imageGallery();
